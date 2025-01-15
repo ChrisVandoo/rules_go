4f5bb87690333fa048400ab9fa42263f2e2533e1
Chris <chris.vandoodewaard@arcticwolf.com>
2025-01-15 12:17:18 -0500

    log

--- go/tools/gopackagesdriver/main.go
+++ go/tools/gopackagesdriver/main.go
@@ -19,6 +19,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"runtime"
 	"strings"
@@ -76,14 +77,15 @@ var (
 )
 
 func run(ctx context.Context, in io.Reader, out io.Writer, args []string) error {
+	l := log.New(os.Stderr, "", 0)
+	l.Println("args: %s", args)
 	queries := args
-
-	return fmt.Errorf("in: %s, args: %s", in, args)
 
 	request, err := ReadDriverRequest(in)
 	if err != nil {
 		return fmt.Errorf("unable to read request: %w", err)
 	}
+	l.Println("request: %s", request)
 
 	bazel, err := NewBazel(ctx, bazelBin, workspaceRoot, buildWorkingDirectory, bazelCommonFlags, bazelStartupFlags)
 	if err != nil {
@@ -118,6 +120,7 @@ func run(ctx context.Context, in io.Reader, out io.Writer, args []string) error 
 	if err != nil {
 		return fmt.Errorf("unable to marshal response: %v", err)
 	}
+	l.Println("data: ", data)
 	_, err = out.Write(data)
 	return err
 }
