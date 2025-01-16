d41d034cb1e4951ded4f30deab6009ff35a99acc
Chris <chris.vandoodewaard@arcticwolf.com>
2025-01-16 08:28:18 -0500

    try logging

--- go/tools/gopackagesdriver/main.go
+++ go/tools/gopackagesdriver/main.go
@@ -78,6 +78,8 @@ var (
 func run(ctx context.Context, in io.Reader, out io.Writer, args []string) error {
 	queries := args
 
+	os.Stderr.WriteString("hello world?")
+
 	request, err := ReadDriverRequest(in)
 	if err != nil {
 		return fmt.Errorf("unable to read request: %w", err)
